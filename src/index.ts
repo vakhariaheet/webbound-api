7a75d2c1972cf16a318df754b0b0767f726df06a
Heet Vakharia <heetkv@gmail.com>
2025-02-04 13:46:49 +0530

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
 
+
+app.use(cors());
+app.use(swagger({
+  documentation: {
+    info: {
+      title: "Webbound API",
+      version: "1.0.0",
+
+    },
+  },
+}));
+
+app.get("/", () => {
+  return "Hello World";
+});
+
+app.post("/", ({ body }) => {
+  return body;
+});
+
+
+app.listen(3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
